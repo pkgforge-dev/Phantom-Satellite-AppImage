9ade005ab1e07bdefb6317fceca9fb3a3d362769
Link4Electronics <link4electronics@gmail.com>
2026-03-14 01:15:28 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,7 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/128x128/apps/phantomsatellite.png
+export ICON=https://raw.githubusercontent.com/DCFUKSURMOM/Phantom-Satellite/refs/heads/master/phantomsatellite/branding/unofficial/mozicon128.png
 export DESKTOP=/usr/share/applications/phantomsatellite.desktop
 export DEPLOY_GTK=1
 export DEPLOY_OPENGL=1
