da0d86d04ffbd1c148e59ee790329a3a8dda5b78
Link4Electronics <link4electronics@gmail.com>
2026-03-22 17:54:36 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -31,10 +31,8 @@ if [ "$ARCH" = "x86_64" ]; then
   tar xvf /tmp/phantomsatellite.tar.xz -C /usr/lib
   rm -f /tmp/phantomsatellite.tar.xz
 else
-	#PRE_BUILD_CMDS='echo "options=(!check)" >> ./PKGBUILD' make-aur-package openssl-1.1
-	#PRE_BUILD_CMDS='echo "options=(!check)" >> ./PKGBUILD' make-aur-package python2
-	BUILDENV="!check" make-aur-package openssl-1.1
-	BUILDENV="!check" make-aur-package python2
+	PRE_BUILD_CMDS='sed -i "/^check() {/,/^}/d" ./PKGBUILD' make-aur-package openssl-1.1
+	PRE_BUILD_CMDS='sed -i "/^check() {/,/^}/d" ./PKGBUILD' make-aur-package python2
 	make-aur-package gtk2
 	sed -i -e 's|-O3|-O2|' /etc/makepkg.conf
 	make-aur-package
